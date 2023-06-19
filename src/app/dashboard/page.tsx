1e00f481703c1e0b1503631091de0a20c205a471
VanTuanNB <vantuan26072002@gmail.com>
2023-06-20 00:16:27 +0700

    generate repo

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+'use-client';
+import styles from './dashboard.module.scss';
+function DashboardPage() {
+    return <h1 className={styles.title}>Dashboard Page</h1>;
+}
+
+export default DashboardPage;
