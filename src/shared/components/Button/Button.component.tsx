05bf2dc047756c06224ad4a5e72cbc0b9fa701b1
ShiinnCz <tuannmps19721@fpt.edu.vn>
2023-06-23 00:56:53 +0700

    create reuseable button component

--- src/shared/components/Button/Button.component.tsx
+++ src/shared/components/Button/Button.component.tsx
@@ -1,9 +1,12 @@
 import classnames from 'classnames/bind';
 
 import styles from './Button.module.scss';
+import React from 'react';
 
 const cx = classnames.bind(styles);
 
-export default function Button() {
-    return <button className={cx('test')}>Do Something</button>;
+export default function Button(props:any) {
+    const {children, style, onCLick} = props;
+    return <button className={cx('test')} style={style ? style :{background: 'red', color: '#000'}} onClick = {onClick}>{children}</button>;
 }
+
