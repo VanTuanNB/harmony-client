f101aa4fdb03398297d48406360af849e1b47811
tienbach123 <phantienbach123@gmail.com>
2023-06-23 15:30:07 +0700

    [HMC-03] fix-player-control

--- src/core/layouts/components/PlayerControl/PlayerControl.component.tsx
+++ src/core/layouts/components/PlayerControl/PlayerControl.component.tsx
@@ -8,44 +8,45 @@ const cx = classNames.bind(styles);
 function PlayerControl() {
     return (
         <div className={cx('player-control')}>
-            <div className={cx('songBar')}>
-                <div className={cx('songInfors')}>
-                    <div className={cx('imageContainer')}>
-                        <Image className={cx('image1')} src="" alt="" width={0} height={0} />
+            <div className={cx('song-bar')}>
+                <div className={cx('song-infors')}>
+                    <div className={cx('image-container')}>
+                        <Image className={cx('image1')} src="/images/img1.jpg" alt="" width={60} height={60} />
                     </div>
-                    <div className={cx('songDescription')}>
+                    <div className={cx('song-description')}>
                         <p className={cx('title')}>Một phút</p>
                         <p className={cx('artist')}>Tiến Bách</p>
                     </div>
                 </div>
                 <div className={cx('icons')}>
-                    <i className={cx(' far fa-heart')}></i>
-                    <i className={cx(' fas fa-compress')}></i>
+                    <i className={cx(styles.icon, 'far', 'fa-heart')}></i>
+                    <i className={cx(styles.icon, 'fas', 'fa-compress')}></i>
                 </div>
             </div>
-            <div className={cx('progressController')}>
-                <div className={cx('controlButtons')}>
-                    <i className={cx('fas fa-random')}></i>
-                    <i className={cx('fa fa-step-backward')}></i>
-                    <i className={cx('play-pause fas fa-play')}></i>
-                    <i className={cx('fas fa-step-forward')}></i>
-                    <i className={cx('fas fa-undo-alt')}></i>
+            <div className={cx('progress-controller')}>
+                <div className={cx('control-buttons')}>
+                    <i className={cx(styles.icon, 'fas', 'fa-random')}></i>
+                    <i className={cx(styles.icon, 'fa', 'fa-step-backward')}></i>
+                    <i className={cx(styles['play-pause'], 'fas', 'fa-play')}></i>
+                    <i className={cx(styles.icon, 'fas', 'fa-step-forward')}></i>
+                    <i className={cx(styles.icon, 'fas', 'fa-undo-alt')}></i>
                 </div>
-                <div className={cx('progressContainer')}>
+
+                <div className={cx('progress-container')}>
                     <span>0:49</span>
-                    <div className={cx('progressBar')}>
+                    <div className={cx('progress-bar')}>
                         <div className={cx('progress')}></div>
                     </div>
                     <span>3:15</span>
                 </div>
             </div>
-            <div className={cx('otherFeatures')}>
-                <i className={cx('fas fa-list-ul')}></i>
-                <i className={cx('fas fa-desktop')}></i>
-                <div className={cx('volumeBar')}>
-                    <i className={cx('fas fa-volume-down')}></i>
-                    <div className={cx('progressBar')}>
-                        <div className={cx('progress')}></div>
+            <div className={cx('other-features')}>
+                <i className={cx(styles.icon, 'fas', 'fa-list-ul')}></i>
+                <i className={cx(styles.icon, 'fas', 'fa-desktop')}></i>
+                <div className={cx('volume-bar')}>
+                    <i className={cx(styles.icon, 'fas', 'fa-volume-down')}></i>
+                    <div className={cx('progress-bar')}>
+                        <div className={cx('progress-inner')}></div>
                     </div>
                 </div>
             </div>
