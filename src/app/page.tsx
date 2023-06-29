d543f132f2178d30cecc58149e23aa57081657c0
JosephHuy1906 <huydeptrai19062002@gmail.com>
2023-06-29 18:09:47 +0700

    [HMC15] done test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@
 import { ReactNode } from 'react';
 
 import HomePage from '@/core/layouts/pages/Home/Home.component';
-import FormSong from '@/shared/components/Form/FormSong.component'; 
 
 export default function AppPage({ children }: { children: ReactNode }) {
-    // return <HomePage />;
-    return <FormSong />;
+    return <HomePage />;
 
 }
