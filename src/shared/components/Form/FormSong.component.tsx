de67ed06f43f8c62735e27e382fb3165dbd1ebb9
JosephHuy1906 <huydeptrai19062002@gmail.com>
2023-06-29 18:08:09 +0700

    [HMC15] done task create song new

--- src/shared/components/Form/FormSong.component.tsx
+++ src/shared/components/Form/FormSong.component.tsx
@@ -3,61 +3,79 @@ import style from './Form.module.scss';
 import classNames from 'classnames/bind';
 import React, { useState } from 'react';
 import Select from 'react-select';
-
-
+import { useForm, Controller } from 'react-hook-form';
 const cx = classNames.bind(style);
 
-const aquaticCreatures = [
-    { label: 'Shark', value: '123' },
-    { label: 'Dolphin', value: '213' },
-    { label: 'Whale', value: '111' },
-    { label: 'Octopus', value: '222' },
-    { label: 'Crab', value: '333' },
-    { label: 'Lobster', value: '1313' },
+const inputGenres = [
+    { label: 'Lofi', value: '78383b39-7c8d-4788-adb5-ca47e05cee5f' },
+    { label: 'rap', value: 'e6579dcc-9449-4852-b962-cd8bdffc0743' },
+    { label: 'Ballad', value: '401dfd91-5dc9-4062-8bb5-53f529f9727f' },
 ];
+const inputAlbum = [
+    { label: 'Ngày Không Còn Em', value: 'e9cdc1ac-7d99-4283-8d31-88cfe137b604' },
+    { label: 'Ngày Mất Em', value: 'ea2f0e04-73c7-4ed9-a183-d7bf1233e9d5' },
+];
+type ISong = {
+    title: string;
+    publish: string;
+    performers: [];
+    composerReference: string;
+    albumReference: {};
+    genresReference: {};
+    thumbnail: string;
+    fileSong: string;
+};
 
 function FormCreate() {
+    const { handleSubmit, control, register } = useForm<ISong>();
+    const onSubmit = handleSubmit((data) => console.log(data));
     return (
         <div className={cx('form')}>
             <h2>Thêm bài hát mới</h2>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className={cx('col-3')}>
                     <label htmlFor="">Tiêu đề: </label>
-                    <input className={cx('input')} type="text" />
+                    <input className={cx('input')} type="text" {...register('title')} />
                 </div>
                 <div className={cx('col-3')}>
                     <label htmlFor="">Ngày sáng tác: </label>
-                    <input className={cx('input')} type="datetime-local" />
+                    <input className={cx('input')} type="datetime-local" {...register('publish')} />
                 </div>
                 <div className={cx('col-3')}>
                     <label htmlFor="">Sáng tác: </label>
-                    <select id="" name="">
+                    <select {...register('composerReference')}>
                         <option value="1211312">Huy Nguyen</option>
-                        <option value="1211312">Tuan Cao</option>
-
                     </select>
                 </div>
                 <div className={cx('col-3')}>
                     <label htmlFor="">Thể hiện: </label>
-                    <select id="" name="">
+                    <select {...register('performers')}>
                         <option value="1211312">Nguyễn Quang Huy</option>
                     </select>
                 </div>
                 <div className={cx('col-3')}>
                     <label htmlFor="">Thể Loại: </label>
-                    <Select options={aquaticCreatures} isMulti className={cx('select-input')} />
+                    <Controller
+                        name="genresReference"
+                        control={control}
+                        render={({ field }) => <Select {...field} options={inputGenres} isMulti  className={cx('select-input')} />}
+                    />
                 </div>
                 <div className={cx('col-3')}>
-                    <label htmlFor="">Album: </label>
-                    <Select options={aquaticCreatures} isMulti className={cx('select-input')} />
+                    <label htmlFor="">Album:</label>
+                    <Controller
+                        name="albumReference"
+                        control={control}
+                        render={({ field }) => <Select {...field} options={inputAlbum} isMulti   className={cx('select-input')} />}
+                    />
                 </div>
                 <div className={cx('col-3')}>
                     <label htmlFor="">Hình ảnh: </label>
-                    <input type="file" />
+                    <input type="file" {...register('thumbnail')} />
                 </div>
                 <div className={cx('col-3')}>
                     <label htmlFor="">File nhạc: </label>
-                    <input type="file" />
+                    <input type="file" {...register('fileSong')} />
                 </div>
                 <button>Thêm bài hát</button>
             </form>
