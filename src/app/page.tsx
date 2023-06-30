0a6aa6dfd3ba01d215688e3c46a38d54eb014abc
JosephHuy1906 <huydeptrai19062002@gmail.com>
2023-06-30 15:55:09 +0700

    [HMC17] done test task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { ReactNode } from 'react';
 
 import HomePage from '@/core/layouts/pages/Home/Home.component';
-import ComposerPage from '@/core/layouts/pages/Composer/Composer.component';
 
 export default function AppPage({ children }: { children: ReactNode }) {
-    // return <HomePage />;
-    return <ComposerPage />;
+    return <HomePage />;
 
 }
--- src/core/layouts/pages/Composer/Composer.component.tsx
+++ src/core/layouts/pages/Composer/Composer.component.tsx
@@ -54,6 +54,72 @@ const albumList = [
         },
         __v: 0,
     },
+    {
+        _id: 'e9cdc1ac-7d99-4283-8d31-88cfe137b604',
+        title: 'Ngày Không Còn Em',
+        publish: {
+            $date: '2011-10-05T14:48:00.000Z',
+        },
+        thumbnail: '/images/fallback-thumbnail-user.jpg',
+        information: '',
+        composerReference: {
+            _id: 'eqwdsacasc',
+            name: 'Hoài Lâm',
+            nickname: 'hoailam',
+        },
+        listSong: ['1f1ec177-a843-4e39-8734-e23faf7adbba'],
+        createdAt: {
+            $date: '2023-06-10T14:32:48.994Z',
+        },
+        updatedAt: {
+            $date: '2023-06-10T14:35:23.402Z',
+        },
+        __v: 0,
+    },
+    {
+        _id: 'ea2f0e04-73c7-4ed9-a183-d7bf1233e9d5',
+        title: 'Ngày Mất Em',
+        publish: {
+            $date: '2011-10-05T14:48:00.000Z',
+        },
+        thumbnail: '/images/fallback-thumbnail-user.jpg',
+        information: '',
+        composerReference: {
+            _id: 'eqwdsacasc',
+            name: 'Hoài Lâm',
+            nickname: 'hoailam',
+        },
+        listSong: ['42d3d57e-8556-4ea1-9b39-1c78c1a8f6ff'],
+        createdAt: {
+            $date: '2023-06-10T14:33:05.819Z',
+        },
+        updatedAt: {
+            $date: '2023-06-10T14:50:16.106Z',
+        },
+        __v: 0,
+    },
+    {
+        _id: 'ea2f0e04-73c7-4ed9-a183-d7bf1233e9d5',
+        title: 'Ngày Mất Em',
+        publish: {
+            $date: '2011-10-05T14:48:00.000Z',
+        },
+        thumbnail: '/images/fallback-thumbnail-user.jpg',
+        information: '',
+        composerReference: {
+            _id: 'eqwdsacasc',
+            name: 'Hoài Lâm',
+            nickname: 'hoailam',
+        },
+        listSong: ['42d3d57e-8556-4ea1-9b39-1c78c1a8f6ff'],
+        createdAt: {
+            $date: '2023-06-10T14:33:05.819Z',
+        },
+        updatedAt: {
+            $date: '2023-06-10T14:50:16.106Z',
+        },
+        __v: 0,
+    },
 ];
 const mockData = [
     {
