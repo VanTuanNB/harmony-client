e25f90b0cc58e0c8afff5710d46dc52c27b4045b
JosephHuy1906 <huydeptrai19062002@gmail.com>
2023-07-06 17:43:59 +0700

    build file done

---
+++ src/core/layouts/pages/Login/Login.component.tsx
@@ -0,0 +1,13 @@
+import classNames from "classnames/bind";
+import styles from './Login.module.scss'
+
+const cx = classNames.bind(styles);
+
+function LoginPage(){
+
+    return (
+        <h2>Login component</h2>
+    )
+}
+
+export default LoginPage;
