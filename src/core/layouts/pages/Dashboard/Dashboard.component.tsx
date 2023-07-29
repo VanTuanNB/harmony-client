eff3dc37e84ae239104f3195194bd244b09087b3
JosephHuy1906 <huydeptrai19062002@gmail.com>
2023-07-29 22:21:31 +0700

    feat/huy/HMC41-create-popup-upload

--- src/core/layouts/pages/Dashboard/Dashboard.component.tsx
+++ src/core/layouts/pages/Dashboard/Dashboard.component.tsx
@@ -1,11 +1,6 @@
 
 function DashboardPage() {
-    return (
-        <div>
-            Dashboard page
-
-        </div>
-    );
+    return <h1>Dashboard Page Component</h1>;
 }
 
 export default DashboardPage;
