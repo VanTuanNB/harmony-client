0e04a3c47fea3cd3f2c2cd8ff5fe4e2a01d8eb52
VanTuanNB <vantuan26072002@gmail.com>
2023-08-11 22:03:47 +0700

    [HMS-45] fix deploy vercel

--- src/core/redux/rootProvider.component.tsx
+++ src/core/redux/rootProvider.component.tsx
@@ -2,7 +2,17 @@
 import { ReactNode } from 'react';
 import { store } from './store.redux';
 import { Provider } from 'react-redux';
+import AuthLayout from '../layouts/AuthLayout/AuthLayout.component';
+import { usePathname } from 'next/navigation';
+import PrimaryLayout from '../layouts/PrimaryLayout/PrimaryLayout.component';
 
 export default function Providers({ children }: { children: ReactNode }) {
-    return <Provider store={store}>{children}</Provider>;
+    const path = usePathname();
+    const regex = /\/auth\//;
+    const condition = regex.test(path);
+    return (
+        <Provider store={store}>
+            {condition ? <AuthLayout>{children}</AuthLayout> : <PrimaryLayout>{children}</PrimaryLayout>}
+        </Provider>
+    );
 }
